ed3d0a284980053e1f80a5c7675e5bfa4444e495
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2024-04-21 18:24:42 +0200

    fix: vet violation
    log/slog package is not available at go1.20.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- internal/usecase/presenter/echo/healthcheck_presenter.go
+++ internal/usecase/presenter/echo/healthcheck_presenter.go
@@ -1,10 +1,10 @@
 package echo
 
 import (
-	"log/slog"
 	"net/http"
 
 	api_healthcheck "github.com/avisiedo/go-microservice-1/internal/api/http/healthcheck"
+	"github.com/avisiedo/go-microservice-1/internal/infrastructure/logger/slogctx"
 	interactor "github.com/avisiedo/go-microservice-1/internal/interface/interactor"
 	presenter "github.com/avisiedo/go-microservice-1/internal/interface/presenter/echo"
 	"github.com/labstack/echo/v4"
@@ -23,8 +23,10 @@ func NewHealthcheck(i interactor.HealthcheckInteractor) presenter.Healthcheck {
 // Liveness kubernetes probe endpoint
 // (GET /livez)
 func (p *healthcheckPresenter) GetLivez(ctx echo.Context) error {
+	c := ctx.Request().Context()
+	l := slogctx.FromCtx(c)
 	if err := p.interactor.IsLive(); err != nil {
-		slog.Error(err.Error())
+		l.ErrorContext(c, err.Error())
 		return ctx.String(http.StatusInternalServerError, api_healthcheck.NotHealthy)
 	}
 	return ctx.String(http.StatusOK, api_healthcheck.Healthy)
@@ -33,8 +35,10 @@ func (p *healthcheckPresenter) GetLivez(ctx echo.Context) error {
 // Readiness kubernetes probe endpoint
 // (GET /readyz)
 func (p *healthcheckPresenter) GetReadyz(ctx echo.Context) error {
+	c := ctx.Request().Context()
+	l := slogctx.FromCtx(c)
 	if err := p.interactor.IsReady(); err != nil {
-		slog.Error(err.Error())
+		l.ErrorContext(c, err.Error())
 		return ctx.String(http.StatusInternalServerError, api_healthcheck.NotReady)
 	}
 	return ctx.String(http.StatusOK, api_healthcheck.Ready)
