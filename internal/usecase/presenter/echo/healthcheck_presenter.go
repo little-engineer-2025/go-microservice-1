57109322ca950e2ae7ab80c705c65923955d5635
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2024-04-21 03:12:39 +0200

    fixup refactor: healthcheck endpoints
    Add the presenter for the healthchecks.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- internal/usecase/presenter/echo/healthcheck_presenter.go
+++ internal/usecase/presenter/echo/healthcheck_presenter.go
@@ -1,8 +1,10 @@
 package echo
 
 import (
+	"log/slog"
 	"net/http"
 
+	api_healthcheck "github.com/avisiedo/go-microservice-1/internal/api/http/healthcheck"
 	interactor "github.com/avisiedo/go-microservice-1/internal/interface/interactor"
 	presenter "github.com/avisiedo/go-microservice-1/internal/interface/presenter/echo"
 	"github.com/labstack/echo/v4"
@@ -22,16 +24,18 @@ func NewHealthcheck(i interactor.HealthcheckInteractor) presenter.Healthcheck {
 // (GET /livez)
 func (p *healthcheckPresenter) GetLivez(ctx echo.Context) error {
 	if err := p.interactor.IsLive(); err != nil {
-		return ctx.JSON(http.StatusForbidden, err.Error())
+		slog.Error(err.Error())
+		return ctx.String(http.StatusInternalServerError, api_healthcheck.NotHealthy)
 	}
-	return ctx.JSON(http.StatusOK, "Livez")
+	return ctx.String(http.StatusOK, api_healthcheck.Healthy)
 }
 
 // Readiness kubernetes probe endpoint
 // (GET /readyz)
 func (p *healthcheckPresenter) GetReadyz(ctx echo.Context) error {
 	if err := p.interactor.IsReady(); err != nil {
-		return ctx.JSON(http.StatusForbidden, err.Error())
+		slog.Error(err.Error())
+		return ctx.String(http.StatusInternalServerError, api_healthcheck.NotReady)
 	}
-	return ctx.JSON(http.StatusOK, "Readyz")
+	return ctx.String(http.StatusOK, api_healthcheck.Ready)
 }
