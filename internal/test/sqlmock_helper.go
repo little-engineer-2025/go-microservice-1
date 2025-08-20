d883ccaf40c5d8b94fbb0a76281ec9a72d93d932
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-08-20 16:00:41 +0200

    chore: document NewSqlMock helper
    Add comment to the helper function NewSqlMock which help on
    creating sqlmock and a gorm.DB connector for unit testing.
    
    Add an example to accelerate the leverage of the function.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- internal/test/sqlmock_helper.go
+++ internal/test/sqlmock_helper.go
@@ -6,6 +6,14 @@ import (
 	"gorm.io/gorm"
 )
 
+// NewSqlMock create a sqlmock associated to the gorm.DB
+// session parameters.
+// Return A sqlmock and gorm.DB reference on success, or nil, nil and an
+// error with the description about what happened.
+//
+// Example
+//
+//	mockDB, db, err := test.NewSqlMock(&gorm.Session{SkipHooks: true})
 func NewSqlMock(session *gorm.Session) (sqlmock.Sqlmock, *gorm.DB, error) {
 	sqlDB, sqlMock, err := sqlmock.New()
 	if err != nil {
