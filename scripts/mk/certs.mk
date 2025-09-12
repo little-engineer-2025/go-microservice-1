dbbe3ddcf33589d4c4dcb2468984865f0e4beccf
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2025-09-12 17:44:18 +0200

    wip build: generate certificates
    - Add initial template for the certificate input properties.
    - Fix rules to generate the certificates.
    
    TODO
    
    - It is not generating the .cnf file for some reason.
    - It is prompting the attributes for the certificate (likely because
      the above point).
    
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- scripts/mk/certs.mk
+++ scripts/mk/certs.mk
@@ -1,36 +1,65 @@
 ##
-# Helper rules to generate a developer self-signed
-# certificate.
-# Do not forget to add SSL_CERT_DIR when starting
-# your microservice pointing out to your repository
-# to serve requests using the temporary certificate.
+# Helper rules to generate a developer self-signed certificate.
 #
-# Note your system should resolve the value of SERVERNAME
-# to 127.0.0.1 on development environments.
+# Do not forget to add SSL_CERT_DIR when starting your microservice pointing out
+# to your repository to serve requests using the temporary certificate.
 #
-# see: man openssl-x509
+# Note your system should resolve the value of CERT_SERVERNAME to 127.0.0.1 on
+# development environments.
+#
+# see:
+#   man openssl-x509
+#   man x509v3_config
 ##
-CERT_DIR ?= $(PROJECT_DIR)/secrets
-CERT_SERVERNAME ?= example.com
-CERT_ALT_NAME ?= DNS:$(CERT_SERVERNAME),DNS:*.$(CERT_SERVERNAME),IP:127.0.0.1
+CERT_DIR ?= $(PROJECT_DIR)/secrets/certs
+CERT_CNF_TEMPLATE ?= configs/certificate.cnf
+CERT_DN ?= example.com
+# CERT_SERVERNAME ?= something.$(CERT_DN)
+CERT_IP ?= 127.0.0.1
+CERT_ALT_NAME ?= DNS:$(CERT_SERVERNAME),IP:$(CERT_IP)
 
-.PHONY: generate-cert
-generate-cert:  $(CERT_DIR)/$(CERT_SERVERNAME).key $(CERT_DIR)/$(CERT_SERVERNAME).crt  ## Generate a local certificate for development
+CERT_SERVERNAME_LIST ?= api.$(CERT_DN) db.$(CERT_DN) minio.$(CERT_DN) redis.$(CERT_DN)
 
-$(CERT_DIR)/$(CERT_SERVERNAME).key $(CERT_DIR)/$(CERT_SERVERNAME).crt:
-	openssl x509 \
-		-new \
-		-set_issuer "/CN=$(CERT_SERVERNAME)" \
-		-subj "/CN=$(CERT_SERVERNAME)" \
-		-not_before "today" \
-		-days 30 \
-		\
-		-key rsa:4096 \
-		-sha256 \
-		-days 7 \
-		-nodes \
-		-keyout $(CERT_DIR)/$(CERT_SERVERNAME).key \
-		-out $(CERT_DIR)/$(CERT_SERVERNAME).crt \
-		-subj "/CN=$(CERT_SERVERNAME)" \
-		-addext "subjectAltName=$(CERT_ALT_SERVERNAME)"
+.PHONY: cert-gen-all
+cert-gen-all:  ## Generate all the local certificates for the service and infrastructure services
+	@[ "$(CERT_SERVERNAME_LIST)" != "" ] || { echo "error: try 'make cert-gen CERT_SERVERNAME_LIST=\"api.example.com\"': CERT_SERVERNAME_LIST is empty"; exit 1; }
+	for item in $(CERT_SERVERNAME_LIST); do \
+		$(MAKE) cert-gen CERT_SERVERNAME="$$item" || { echo "error: cert-gen-all for $$item"; exit 1; }; \
+	done
+
+.PHONY: cert-gen
+cert-gen: $(CERT_DIR) $(CERT_DIR)/$(CERT_SERVERNAME).key $(CERT_DIR)/$(CERT_SERVERNAME).crt  ## Generate a local certificate for development for CERT_SERVERNAME fqdn
+
+$(CERT_DIR):
+	mkdir -p "$(CERT_DIR)"
+
+# $(CERT_DIR)/%.key $(CERT_DIR)/%.crt: $(CERT_DIR)/%.cnf
+#	exit 1
+#	openssl req -x509 -nodes -days 7 -newkey rsa:2048 -keyout "$(CERT_DIR)/$(CERT_SERVERNAME).key" -out "$(CERT_DIR)/$(CERT_SERVERNAME).crt" -config "$(CERT_DIR)/$(CERT_SERVERNAME).cnf"
+
+$(CERT_DIR)/%.key $(CERT_DIR)/%.csr $(CERT_DIR)/%.key: $(CERT_DIR)/%.cnf
+	openssl req -new \
+		-config "$(CERT_DIR)/$(CERT_SERVERNAME).cnf" \
+		-keyout "$(CERT_DIR)/$(CERT_SERVERNAME).key" \
+		-out "$(CERT_DIR)/$(CERT_SERVERNAME).csr" \
+		-nodes
+
+$(CERT_DIR)/%.crt: $(CERT_DIR)/%.key $(CERT_DIR)/%.csr
+	openssl x509 -req \
+		-config "$(CERT_DIR)/$(CERT_SERVERNAME).cnf" \
+		-days 365 \
+		-in "$(CERT_DIR)/$(CERT_SERVERNAME).csr" \
+		-signkey "$(CERT_DIR)/$(CERT_SERVERNAME).key" \
+		-out "$(CERT_DIR)/$(CERT_SERVERNAME).crt"
+
+$(CERT_DIR)/%.cnf:
+	cat "$(CERT_CNF_TEMPLATE)" \
+		| sed -e "r/commonName =/commonName = $(CERT_SERVERNAME)/g" \
+		| sed -e "r/countryName =/countryName = US/g" \
+		| sed -e "r/stateOrProvinceName =/stateOrProvinceName = California/g" \
+		| sed -e "r/localityName =/localityName = San Francisco/g" \
+		| sed -e "r/organizationName =/organizationName = My Company/g" \
+		| sed -e "r/DNS.1 =/DNS.1 = $(CERT_SERVERNAME)/g" \
+		| sed -e "r/IP.1 =/IP.1 = $(CERT_IP)/g" > "$(CERT_DIR)/$(CERT_SERVERNAME).cnf" \
+		| tee "$@"
 
